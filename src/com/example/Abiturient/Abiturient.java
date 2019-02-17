4665131bd7854df97af42e2738acae3ac21575f9
Rimlin <Rimlin@gmail.com>
2019-02-17 18:32:10 +0300

    Abiturient oldVer CorrectVersion

--- src/com/example/Abiturient/Abiturient.java
+++ src/com/example/Abiturient/Abiturient.java
@@ -1,24 +1,21 @@
 package com.example.Abiturient;
 
-import java.text.DecimalFormat;
+
+import java.util.Objects;
 
 public class Abiturient {
 
-   private int id;
-  private   String lastName;
-  private   String firstName;
-   private String patronymic;
-   private String address;
-   private String telephone;
-   private String ratings;
-   private Double excellentRating;
-   private Double countRating;
-public String[][] abiturient = new String[8][17];
-public Double[][] exRating = new Double[2][17];
-    public Abiturient(String lastName, String firstName, String patronymic, String address, int i, int i1, double excellentRating) {
-    }
+    private int id;
+    private String lastName;
+    private String firstName;
+    private String patronymic;
+    private String address;
+    private String telephone;
+    private String ratings;
+    private String excellentRating;
 
-    public Abiturient( String lastName, String firstName, String patronymic, String address, String telephone, String ratings, Double excellentRating) {
+
+    public Abiturient(String lastName, String firstName, String patronymic, String address, String telephone, String ratings, String excellentRating) {
 
         this.lastName = lastName;
         this.firstName = firstName;
@@ -29,103 +26,64 @@ public Double[][] exRating = new Double[2][17];
         this.excellentRating = excellentRating;
     }
 
-    public Abiturient() {
-    }
 
-    public Double getExRating() {
 
-        excellentRating=(exRating[0][getId()]);
-      return excellentRating;
-   }
-
-    public void setExRating(Double excellentRating) {
-        getId();
-        this.excellentRating = excellentRating;
-       exRating[0][getId()]=excellentRating;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getLastName() {
-        getId();
-       lastName = abiturient[1][getId()];
         return lastName;
     }
 
-
-    public void setLastName(String lastName) {
-        lastName = lastName;
-        abiturient[1][getId()] = lastName;
-    }
-
     public String getFirstName() {
-
-        firstName = abiturient[2][getId()];
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        firstName = firstName;
-        abiturient[3][getId()] = firstName;
-    }
-
     public String getPatronymic() {
-        patronymic = abiturient[3][getId()];
         return patronymic;
     }
 
-    public void setPatronymic(String patronymic) {
-        patronymic = patronymic;
-        abiturient[3][getId()]=patronymic;
-    }
-
     public String getAddress() {
-        address =abiturient[4][getId()];
         return address;
     }
 
-    public void setAddress(String address) {
-        address = address;
-    }
-
     public String getTelephone() {
-        telephone = abiturient[5][getId()];
         return telephone;
     }
 
-    public void setTelephone(String Telephone) {
-        Telephone = Telephone;
-    }
-
     public String getRatings() {
-        ratings=abiturient[6][getId()];
         return ratings;
     }
 
-    public void setRatings(String ratings) {
-        this.ratings = ratings;
+    public String getExcellentRating() {
+        return excellentRating;
     }
 
-    public void initArray() {
-        String fieldId = "0 ; 1 ; 2 ; 3 ; 4 ; 5 ; 6 ; 7 ; 8 ; 9 ; 10 ; 11 ; 12 ; 13 ; 14 ; 15 ; 16 ";
-        String fieldLastName = "Сидоров ; Брундуков ; Верещако ; Голотовский ; Добринская  ; Жуковин  ; Зуева ; Иванченков ; Игнатович ; Казаченок ; Коверзюк ; Козак ; Кот ; Кочеткова ; Кривецкий ; Марчик ; Мирный ;";
-        String fieldFirstName = "Петр ; Артем ; Владислав ; Никита ; Дарья ; Леонид ; Вероника ; Артур ; Диана ; Галина ; Мария ; Александр ; Юлия ; Лилия ; Александр ; Виктория ; Владислав ; ";
-        String fieldPatronymic = "Витальевич ; Александрович ; Вячеславович ; Владимирович ; Дмитриевна ; Максимович ; Вячеславовна ; Андреевич ; Викторовна ; Валерьевна ; Игоревна ; Николаевич ; Александровна ; Геннадьевна ; Александрович ; Леонидовна ; Иванович ; ";
-        String fieldAddress = "ул.Крапоткина ; ул.Давида ; ул.Лебедя ; ул.Крапоткина ; ул.Сепаратистов ; ул.Давида ; ул.Опереты ; ул.Крапоткина ; ул.Лебедя ; ул.Красина ; ул.Миронова ; ул.Заграницы ; ул.Павленко ; ул.Космонавтов ; ул.Крапоткина ; ул.Красной Армии ; ул.Миронова ";
-        String fieldTelephone = "345345 ; 6785454 ; 5455667 ; 893434 ; 233568 ; 9783242 ; 6797833 ; 345345 ; 780978235 ; 345343 ; 345345 ; 3453455 ; 98734534 ; 567878 ; 3343664 ; 6767866 ; 2342342 ";
-        String fieldRating = "3 4 5 3 7 8 2 ; 4 2 3 9 5 4 5 ; 9 2 3 4 1 4 6 ; 2 3 5 2 9 3 5 ; 9 8 7 5 6 7 4 ; 4 5 7 8 4 5 6 ; 5 6 4 5 6 4 9 ; 5 6 5 6 8 5 6 ; 4 5 4 7 5 6 8 ; 5 4 5 4 4 5 6 ; 4 3 5 6 4 5 7 ; 5 4 5 6 4 5 6 ; 4 5 4 5 6 4 5 ; 4 5 4 5 6 4 5 ; 4 5 4 5 6 4 5 ; 7 8 9 6 7 8 6 ; 6 7 8 6 7 4 5 ";
-        abiturient[0] = fieldId.split(" ; ");
-        abiturient[1] = fieldLastName.split(" ; ");
-        abiturient[2] = fieldFirstName.split(" ; ");
-        abiturient[3] = fieldPatronymic.split(" ; ");
-        abiturient[4] = fieldAddress.split(" ; ");
-        abiturient[5] = fieldTelephone.split(" ; ");
-        abiturient[6] = fieldRating.split(" ; ");
+    public void setExcellentRating(String excellentRating) {
+        this.excellentRating = excellentRating;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Abiturient)) return false;
+        Abiturient abiturient = (Abiturient) o;
+        return Objects.equals(getLastName(), abiturient.getLastName()) &&
+                Objects.equals(getFirstName(),abiturient.getFirstName()) &&
+                        Objects.equals(getPatronymic(), abiturient.getPatronymic())&&
+                        Objects.equals(getAddress(),abiturient.getAddress())&&
+                                Objects.equals(getTelephone(), abiturient.getTelephone())&&
+                                        Objects.equals(getRatings(), abiturient.getRatings())&&
+                                        Objects.equals(getExcellentRating(),abiturient.getExcellentRating());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getLastName(), getFirstName(), getPatronymic(),getAddress(), getTelephone(), getRatings(), getExcellentRating());
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Студент " +lastName + " " +firstName+" " + excellentRating;
     }
 }
--- src/com/example/Abiturient/Main.java
+++ src/com/example/Abiturient/Main.java
@@ -21,133 +21,109 @@ public class Main {
     }
 
 
-
     public static void main(String[] args) {
 
+        List<Abiturient> abiturients = makeAbiturient();
 
 
-
-
-
-
-        Main person = new Main();
-        Abiturient person1 = new Abiturient();
-
-
-        Double exRating = person1.getExRating();
-
-        // Double exRating = person1.getExRating();
-        person1.setExRating(exRating);
-
-        String Patronymic = person1.getPatronymic();
-        person1.setPatronymic(Patronymic);
-        String Address = person1.getAddress();
-        person1.setAddress(Address);
-        String Telephone = person1.getTelephone();
-        person1.setTelephone(Telephone);
-        String ratings = person1.getRatings();
-        person1.setRatings(ratings);
-        String FirstName = person1.getFirstName();
-        person1.setFirstName(FirstName);
-        String LastName = person1.getLastName();
-        person1.setLastName(LastName);
-        int Id = person1.getId();
-        person1.setId(Id);
-
-        person1.initArray();
-        person.averageRating(person1);
+        makeAbiturient();
+        // show1(abiturients);
+        // counter(abiturients);
+       // averageRating(abiturients);
         // person.test(person1);
-       // person.excellentAbiturient(person1);
-        // person.sortArray(person1);
+        //excellentAbiturient(abiturients);
+        sortArray(abiturients);
 
-        // person.badRatings(person1);
+        // badRatings(abiturients);
         // person.show(person1);
 
     }
 
-    private static List<Abiturient>  makeAbiturient(Abiturient abiturient){
-        List<Abiturient> abiturients= new LinkedList<Abiturient>();
-        abiturients.add(new Abiturient("Сидоров","Петр","Витальевич","ул.Крапоткина",345345,3+ 4+ 5 +3 +7+ 8+ 2, 4.57));
-        abiturients.add(new Abiturient("Брундуков"," Артем","Александрович"," ул.Давида",6785454, 4 +2+ 3+ 9 +5 +4 +9,5.14));
-        abiturients.add(new Abiturient("Голотовский","Никита","Владимирович","ул.Лебедя",5455667,9 +2+ 3+ 4 +1 +4 +6,4.14));
-        abiturients.add(new Abiturient("Верещако","Владислав","Максимович","ул.Крапоткина",893434,2 +3+ 5+ 2+ 9 +3 +5,4.14));
-        abiturients.add(new Abiturient("Добринская","Дарья","Дмитриевна","ул.Сепаратистов",233568,9+ 8+ 7 +5+ 6 +7+ 4,6.57));
-        abiturients.add(new Abiturient("Жуковин","Леонид","Вячеславович"," ул.Давида",9783242,9 +2+ 3+ 4 +1 +4 +6,4.14));
-        abiturients.add(new Abiturient("Верещако","Владислав","Вячеславович","ул.Лебедя",5455667,4 +5+ 7+ 8+ 4+ 5+ 6,5.57));
-        abiturients.add(new Abiturient("Зуева","Вероника","Вячеславовна","ул.Опереты",6797833,5 +6+ 4+ 5+ 6+ 4+ 9,5.57));
-        abiturients.add(new Abiturient("Иванченков","Артур","Андреевич","ул.Крапоткина",345345,5 +6+ 5+ 6+ 8+ 5+ 6,5.85));
-        abiturients.add(new Abiturient("Игнатович","Диана","Вячеславович","ул.Лебедя",780978235,4 +5+ 5+ 7+ 5+ 6+ 8,5.58));
-        abiturients.add(new Abiturient("Казаченок","Галина","Валерьевна","ул.Красина",345343, 5+ 4+ 5+ 4+ 4+ 5+ 6,4.71));
-        abiturients.add(new Abiturient("Коверзюк","Мария","Игоревна","ул.Лебедя",5455667,9 +2+ 3+ 4 +1 +4 +6,4.14));
-        abiturients.add(new Abiturient("Козак","Александр","Николаевич","ул.Заграницы",5345345,7+ 8+ 9+ 6+ 7+ 8+ 6,7.28));
-        abiturients.add(new Abiturient("Кот","Юлия","Александровна","ул.Павленко",98734534,4+ 5+ 4+ 5+ 6+ 4+ 5,4.71));
-        abiturients.add(new Abiturient("Кочеткова","Лилия","Геннадьевна","ул.Космонавтов",567878,4 +5+ 9+ 5+ 6+ 4+ 5,5.42));
-        abiturients.add(new Abiturient("Кривецкий","Александр","Александрович","ул.Крапоткина",3343664,5 +5+ 9+ 5+ 6+ 7+ 5,6));
-        abiturients.add(new Abiturient("Марчик","Виктория","Леонидовна","ул.Красной Армии",6767866,4 +5+ 9+ 5+ 6+ 4+ 5,5.42));
-        abiturients.add(new Abiturient("Мирный","Владислав","Иванович"," ул.Миронова", 2342342,6 +7+ 8+ 6+ 7+ 4+ 5,6.14));
-return abiturients;
+    private static List<Abiturient> makeAbiturient() {
+        List<Abiturient> abiturients = new LinkedList<>();
+        abiturients.add(new Abiturient("Сидоров", "Петр", "Витальевич", "ул.Крапоткина", "345345", "3 +4 +5 +3 +7 +8 +2", null));
+        abiturients.add(new Abiturient("Брундуков", " Артем", "Александрович", " ул.Давида", "6785454", "4 +2 +3 +9 +5 +4 +9", null));
+        abiturients.add(new Abiturient("Голотовский", "Никита", "Владимирович", "ул.Лебедя", "5455667", "9 +2 +3 +4 +1 +4 +6", null));
+        abiturients.add(new Abiturient("Верещако", "Владислав", "Максимович", "ул.Крапоткина", "893434", "2 +3 +5 +2 +9 +3 +5", null));
+        abiturients.add(new Abiturient("Добринская", "Дарья", "Дмитриевна", "ул.Сепаратистов", "233568", "9 +8 +7 +5 +6 +7 +4", null));
+        abiturients.add(new Abiturient("Жуковин", "Леонид", "Вячеславович", " ул.Давида", "9783242", "9 +2 +3 +4 +1 +4 +6", null));
+        abiturients.add(new Abiturient("Верещако", "Владислав", "Вячеславович", "ул.Лебедя", "5455667", "4 +5 +7 +8 +4 +5 +6", null));
+        abiturients.add(new Abiturient("Зуева", "Вероника", "Вячеславовна", "ул.Опереты", "6797833", "5 +6 +4 +5 +6 +4 +9", null));
+        abiturients.add(new Abiturient("Иванченков", "Артур", "Андреевич", "ул.Крапоткина", "345345", "5 +6 +5 +6 +8 +5 +6", null));
+        abiturients.add(new Abiturient("Игнатович", "Диана", "Вячеславович", "ул.Лебедя", "780978235", "4 +5 +5 +7 +5 +6 +8", null));
+        abiturients.add(new Abiturient("Казаченок", "Галина", "Валерьевна", "ул.Красина", "345343", "5 +4 +5 +4 +4 +5 +6", null));
+        abiturients.add(new Abiturient("Коверзюк", "Мария", "Игоревна", "ул.Лебедя", "5455667", "9 +2 +3 +4 +1 +4 +6", null));
+        abiturients.add(new Abiturient("Козак", "Александр", "Николаевич", "ул.Заграницы", "5345345", "7 +8 +9 +6 +7 +8 +6", null));
+        abiturients.add(new Abiturient("Кот", "Юлия", "Александровна", "ул.Павленко", "98734534", "4 +5 +4 +5 +6 +4 +5", null));
+        abiturients.add(new Abiturient("Кочеткова", "Лилия", "Геннадьевна", "ул.Космонавтов", "567878", "4 +5 +9 +5 +6 +4 +5", null));
+        abiturients.add(new Abiturient("Кривецкий", "Александр", "Александрович", "ул.Крапоткина", "3343664", "5 +5 +9 +5 +6 +7 +5", null));
+        abiturients.add(new Abiturient("Марчик", "Виктория", "Леонидовна", "ул.Красной Армии", "6767866", "4 +5 +9 +5 +6 +4 +5", null));
+        abiturients.add(new Abiturient("Мирный", "Владислав", "Иванович", " ул.Миронова", "2342342", "6 +7 +8 +6 +7 +4 +5", null));
+        return abiturients;
     }
 
 
-    private void show(Abiturient person) {
-        for (int i = 0; i < 16; i++) {
+    private static void show1(List<Abiturient> abiturients) {
+        for (Abiturient abiturient : abiturients) {
 
-            person.setId(i);
-            System.out.println(person.getLastName() + " " + person.getFirstName() + " " + person.getPatronymic() + " " +
-                    person.getAddress() + " " + person.getTelephone() + " " + " " + person.getRatings());
+
+            System.out.println(abiturient.getLastName() + " " + abiturient.getFirstName() + " " + abiturient.getPatronymic() + " " +
+                    abiturient.getAddress() + " " + abiturient.getTelephone() + " " + " " + abiturient.getRatings());
 
         }
     }
 
-    private void badRatings(Abiturient person) {
-        int counter = 17;
-        for (int j = 0; j <= 16; j++) {
+    private static void badRatings(List<Abiturient> abiturients) {
+
+        String[] str = null;
+        for (Abiturient abiturient : abiturients) {
+            int counter = 17;
+            str = abiturient.getRatings().split(" +");
             for (int i = 0; i < str.length; i++) {
-                person.setId(j);
-                str = person.getRatings().split(" ");
-                if (Integer.parseInt(str[i]) <= 2 && counter != j) {
-                    counter = j;
-                    System.out.println(person.getLastName() + " " + person.getFirstName() + " " + person.getRatings());
+                if (Integer.parseInt(str[i]) <= 2 && counter == 17) {
+                    counter--;
+                    System.out.println(abiturient.getLastName() + " " + abiturient.getFirstName());
                 }
             }
+
         }
     }
 
-    private void averageRating(Abiturient person) {
-        str1 = "";
+
+    private static void averageRating(List<Abiturient> abiturients) {
+        String[] str = null;
         System.out.println("Введите средний бал");
         double n = inputDouble();
         DecimalFormat count = new DecimalFormat("#0.00");
         double counter;
-        for (int j = 0; j < 17; ++j) {
+        for (Abiturient abiturient : abiturients) {
             counter = 0;
-            counter = getCounter(person, j, counter);
+            str = abiturient.getRatings().split(" +");
+            for (int i = 0; i < str.length; i++) {
+                counter = (counter + (Integer.parseInt(str[i])));
+            }
+            String exellens = count.format(counter / str.length);
+
             if ((counter / str.length) > n) {
-                str1 = str1 + (person.getId()) + " ; ";
-                System.out.println(person.getLastName() + " " + person.getFirstName() + "  средний бал " + count.format(counter / str.length));
+
+                System.out.println(abiturient.getLastName() + " " + abiturient.getFirstName() + "  средний бал " + count.format(counter / str.length));
             }
         }
     }
 
-
-    private void sortArray(Abiturient person) {
-        for (int i = 0; i < 17; i++) {
-            for (int j = 0; j < 17 - 1; j++) {
-                if (person.exRating[0][j] < person.exRating[0][j + 1]) {
-                    double n = person.exRating[0][j];
-                    double n1 = person.exRating[1][j];
-                    person.exRating[0][j] = person.exRating[0][j + 1];
-                    person.exRating[1][j] = person.exRating[1][j + 1];
-                    person.exRating[0][j + 1] = n;
-                    person.exRating[1][j + 1] = n1;
-                }
-
+    private static void sortArray(List<Abiturient> abiturients) {
+        counter( abiturients);
+        Comparator<Abiturient> comparator = Comparator.comparing(abiturient -> abiturient.getExcellentRating());
+        Collections.sort(abiturients, Collections.reverseOrder(comparator));
+        for (Abiturient abiturient:abiturients) {
+            System.out.println(abiturient.getFirstName()+ " "+ abiturient.getLastName()+" "+ abiturient.getExcellentRating());
+        }
             }
 
-        }
-    }
 
-    private int inputInt() {
+
+
+    private static int inputInt() {
         int num1 = 0;
         Scanner sc = new Scanner(System.in);
         if (sc.hasNextInt()) {
@@ -156,7 +132,8 @@ return abiturients;
         return num1;
     }
 
-    private double inputDouble() {
+    //
+    private static double inputDouble() {
         double num = 0;
         Scanner sc = new Scanner(System.in);
         if (sc.hasNextDouble()) {
@@ -165,63 +142,73 @@ return abiturients;
         return num;
     }
 
-    private void excellentAbiturient(Abiturient person) {
+
+    private static void excellentAbiturient(List<Abiturient> abiturients) {
         DecimalFormat count = new DecimalFormat("#0.00");
         String st = "";
         double n2 = 0;
         double n1;
-        str1 = "";
+        String[] str = null;
         double n = 0;
         int n3 = 0;
-
-        for (int j = 0; j < 17; ++j) {
-            double counter = 0;
-            counter = getCounter(person, j, counter);
-
-            // person1.setcountRating((double) i);
-            n2 = n2 + (counter / str.length);
-            // person.setExRating(counter / str.length);
-            // excellent[0][j] = "" + (counter / str.length);
-            // excellent[1][j] = "" + j;
-            person.exRating[0][j] = (counter / str.length);
-            person.exRating[1][j] = ((double) j);
+        double counter = 0;
+        for (Abiturient abiturient : abiturients) {
+            counter = 0;
+            str = abiturient.getRatings().split(" +");
+            for (int i = 0; i < str.length; i++) {
+                counter = (counter + (Integer.parseInt(str[i])));
+            }
+            n1 = counter / str.length;
+            abiturient.setExcellentRating(""+n1);
         }
-        System.out.print("проходной бал =  5,58");
-        System.out.println();
-        System.out.println("Введите кол-во студентов с балом больше проходного");
-        n1 = inputInt();
+            System.out.print("проходной бал =  5,58");
+            System.out.println();
+            System.out.println("Введите кол-во студентов с балом больше проходного");
+            n3 = inputInt();
+            Comparator<Abiturient> comparator = Comparator.comparing(abiturient -> abiturient.getExcellentRating());
+            Collections.sort(abiturients, Collections.reverseOrder(comparator));
 
-        sortArray(person);
-        // test(person);
-        for (int i = 0; i < n1; i++) {
-            n = person.exRating[1][i];
-            n3 = (int) n;
-            person.setId(n3);
-            if (person.exRating[0][i] > (5.58)) {
-                System.out.println(person.getLastName() + " " + count.format(person.exRating[0][i]));
+        for (Abiturient abiturient:abiturients ) {
+st=(abiturient.getExcellentRating());
+n1=Double.parseDouble(st);
+            //abiturient.setExcellentRating(count.format(n1));
+            if (n1 >= 5.58 && n3!=0) {
+                n3--;
 
+                System.out.println(abiturient.getFirstName() + " " + abiturient.getExcellentRating());
             }
         }
-        n3=0; System.out.println("Осталось одно место с полупроходным балом 5.57, на которое претендуют ");
-        for (int i1 = 0; i1 < 17; i1++) {
-            person.setId(i1);
-            if (person.exRating[0][i1] <= 5.58&&n3<3) {n3++;
 
-                System.out.println( person.getLastName() + "с балом " + count.format(person.exRating[0][i1]));
+        n3 = 0;
+        System.out.println("Осталось одно место с полупроходным балом 5.57, на которое претендуют ");
+        for (Abiturient abiturient:abiturients ) {
+            st=(abiturient.getExcellentRating());
+            n1=Double.parseDouble(st);
+            //abiturient.setExcellentRating(count.format(n1));
+            if (n1 <= 5.58 && n3 < 3) {
+                n3++;
+
+                System.out.println(abiturient.getLastName() + " с балом " + abiturient.getExcellentRating());
             }
         }
     }
-    private double getCounter(Abiturient person, int j, double counter) {
-        person.setId(j);
-        str = person.getRatings().split(" ");
-        for (int i = 0; i < str.length; i++) {
-            counter = (counter + Integer.parseInt(str[i]));
+
+    private static void counter(List<Abiturient> abiturients) {
+        DecimalFormat count = new DecimalFormat("# 0.00");
+        double counter = 0;
+        String[] str = null;
+        Abiturient str1 = null;
+        for (Abiturient abiturient : abiturients) {
+            counter = 0;
+            str = abiturient.getRatings().split(" +");
+            for (int i = 0; i < str.length; i++) {
+                counter = (counter + (Integer.parseInt(str[i])));
+            }
+            String exellens = count.format(counter / str.length);
+            abiturient.setExcellentRating(exellens);
+           // System.out.println(abiturient.getFirstName() + " " + abiturient.getExcellentRating());
+
+
         }
-        return counter;
     }
-
-
 }
-
-
-
